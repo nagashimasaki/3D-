2bd6f08f43c28e12b097a03f4062c8a293e9ee2f
nagashima0112 <mski4649@icloud.com>
2021-05-11 21:13:05 +0900

    手順の5まで終了

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // カメラが追従する対象のゲームオブジェクト。今回はペンギン
+    [SerializeField]
+    private GameObject playerObj;  
+
+    // カメラが追従する対象との間の取る、一定の距離用の補正値
+    private Vector3 offset;         
+
+    void Start()
+    {
+        // カメラと追従対象のゲームオブジェクトとの距離を補正値として取得
+        offset = transform.position - playerObj.transform.position;
+    }
+
+    void Update()
+    {
+        // 追従対象がいる場合
+        if (playerObj != null)
+        {
+            // カメラの位置を追従対象の位置 + 補正値にする
+            transform.position = playerObj.transform.position + offset;
+        }
+    }
+}
