71446cc20ffc4cf6accb900f519b00c34d2726ca
nagashima0112 <mski4649@icloud.com>
2021-05-12 10:32:08 +0900

    手順の10まで終了

---
+++ Assets/Scripts/FlowerCircle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class FlowerCircle : MonoBehaviour
+{
+    void Start()
+    {
+
+        // アタッチしたゲームオブジェクト(花輪)を回転させる
+        transform.DORotate(new Vector3(0, 360, 0), 5.0f, RotateMode.FastBeyond360).SetEase(Ease.Linear).SetLoops(-1, LoopType.Restart);
+    }
+}
