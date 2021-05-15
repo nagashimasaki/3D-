fc325802570bc499ca5e41fef9234bffb68a5db4
nagashima0112 <mski4649@icloud.com>
2021-05-15 23:52:40 +0900

    手順の25まで終了（Canvas内にUI部品を作成する⑥（クリア表示）、スクリプトを使って、ゲームクリアの際にクリア表示をする処理と、クリックで最初からリスタートする処理を追加する）
    ・DOTweenの補間機能と実装例⑤　ーOnCompleteメソッドー

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -18,6 +18,9 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private CameraController cameraController;
 
+    [SerializeField]
+    private ResultPopUp resultPopUp;
+
     // キャラと水面までの距離の計測用
     private float distance;
 
@@ -37,27 +40,25 @@ public class GameManager : MonoBehaviour
         // Y軸が高さの情報なので、双方の高さの値を減算して差分値を距離とする
         distance = player.transform.position.y - goal.position.y;
 
-        // 距離が 0 以下になったら(条件を distance == 0 とはしないようにします。なぜなら、distance の値が 0 ピッタリに値が止まることが出来ないためです)
+        // 距離が 0 以下になったら
         if (distance <= 0)
         {
 
             // 距離が 0 以下になったので、ゴールと判定する
             isGoal = true;
 
-            // 距離を 0 にする(この処理がないと、distance の値がマイナス方向になります。試しに書かないで処理を実行してみましょう)
+            // 距離を 0 にする
             distance = 0;
 
-            // TODO 着水した際の処理を追加する
+            // カメラを初期のカメラに戻す
+            cameraController.SetDefaultCamera();
+
+            // リザルト表示
+            resultPopUp.DisplayResult();
 
         }
-
         // Consoleビューに距離を表示する
         //Debug.Log(distance.ToString("F2"));
-
         txtDistance.text = distance.ToString("F2");
-
-        // カメラを初期のカメラに戻す
-        cameraController.SetDefaultCamera();
-
     }
 }
