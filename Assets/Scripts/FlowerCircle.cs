32b4cfe5fc6b9db2a11ae9a8849cc84f7f508384
nagashima0112 <mski4649@icloud.com>
2021-05-18 21:05:54 +0900

    発展1
    ・花輪の自動生成の制御処理を追加する
    ・Range属性
    ・if文の条件式に戻り値を持つメソッドを利用する場合の実装例
    ・Startメソッドの戻り値をvoidからIEnumerator型にして使用する処理
    ・yield return StartCoroutine()処理による待機処理
    ・GameObject 型ではなくて自作クラスでプレファブをインスタンスする方法
    ・引数に条件式を利用する方法

--- Assets/Scripts/FlowerCircle.cs
+++ Assets/Scripts/FlowerCircle.cs
@@ -14,9 +14,8 @@ public class FlowerCircle : MonoBehaviour
     [SerializeField]
     private GameObject effectPrefab;
 
-    // 通過時と獲得時で２回別々のSEを鳴らしたい場合には変数を２つ用意する
     [SerializeField]
-    private AudioClip flowerSE;　　
+    private AudioClip flowerSE;　　// 通過時と獲得時で２回別々のSEを鳴らしたい場合には変数を２つ用意する
 
     [SerializeField, Header("移動させる場合スイッチ入れる")]
     private bool isMoveing;
@@ -27,25 +26,42 @@ public class FlowerCircle : MonoBehaviour
     [SerializeField, Header("移動距離")]
     private float moveDistance;
 
+
+    [SerializeField, Header("移動する時間と距離をランダムにする割合"), Range(0, 100)]
+    private int randomMovingPercent;
+
+    [SerializeField, Header("移動時間のランダム幅")]
+    private Vector2 durationRange;
+
+    [SerializeField, Header("移動距離のランダム幅")]
+    private Vector2 moveDistanceRange;
+
+    [SerializeField, Header("大きさの設定")]
+    private float[] flowerSizes;
+
+    [SerializeField, Header("点数の倍率")]
+    private float[] pointRate;
+
     void Start()
     {
+
         // アタッチしたゲームオブジェクト(花輪)を回転させる
         transform.DORotate(new Vector3(0, 360, 0), 5.0f, RotateMode.FastBeyond360).SetEase(Ease.Linear).SetLoops(-1, LoopType.Restart);
 
         // この花輪が移動する花輪の設定なら
         if (isMoveing)
         {
+
             // 前後にループ移動させる
             transform.DOMoveZ(transform.position.z + moveDistance, duration).SetEase(Ease.Linear).SetLoops(-1, LoopType.Yoyo);
         }
     }
 
-    /// <summary>
-    /// 花輪からみて、他のゲームオブジェクトが花輪に侵入した場合
-    /// </summary>
-    /// <param name="other"></param>
+
+    // 花輪からみて、他のゲームオブジェクトが花輪に侵入した場合
     private void OnTriggerEnter(Collider other)
     {
+
         // 水面に触れても通過判定は行わない
         if (other.gameObject.tag == "Water")
         {
@@ -58,7 +74,6 @@ public class FlowerCircle : MonoBehaviour
         // 花輪をキャラの子オブジェクトにする
         transform.SetParent(other.transform);
 
-        // コルーチンメソッドの呼び出し命令に変更する
         // 花輪をくぐった際の演出
         StartCoroutine(PlayGetEffect());
     }
@@ -87,7 +102,7 @@ public class FlowerCircle : MonoBehaviour
         // エフェクトを生成して、Instantiate メソッドの戻り値を effect 変数に代入
         GameObject effect = Instantiate(effectPrefab, transform.position, Quaternion.identity);
 
-        // エフェクトの位置(高さ)を調整する　
+        // エフェクトの位置(高さ)を調整する　<=　☆　この高さの調整が必要な理由はなんだと思いますか？　一度、この処理をコメントアウトして、どのような違いが出るか確認してください。
         effect.transform.position = new Vector3(effect.transform.position.x, effect.transform.position.y - 1.5f, effect.transform.position.z);
 
         // 1秒後にエフェクトを破棄(すぐに破棄するとエフェクトがすべて再生されないため)
@@ -98,5 +113,69 @@ public class FlowerCircle : MonoBehaviour
 
         // 花輪を1秒後に破棄
         Destroy(gameObject, 1.0f);
+    }
+
+    /// <summary>
+    /// 移動する花輪の設定
+    /// </summary>
+    public void SetUpMovingFlowerCircle(bool isMoving, bool isScaleChanging)
+    {
+        // 移動する花輪か、通常の花輪かの設定
+        isMoving = isMoving;
+
+        // 移動する場合
+        if (isMoving)
+        {
+            // ランダムな移動時間や距離を使うか、戻り値を持つメソッドを利用して判定
+            if (DetectRandomMovingFromPercent())
+            {
+                // ランダムの場合には、移動時間と距離のランダム設定を行う
+                ChangeRandomMoveParameters();
+            }
+        }
+
+        // 花輪の大きさを変更する場合
+        if (isScaleChanging)
+        {
+            // 大きさを変更
+            ChangeRandomScales();
+        }
+    }
+    
+    /// <summary>
+    /// 移動時間と距離をランダムにするか判定。true の場合はランダムとする
+    /// </summary>
+    /// <returns></returns>
+    private bool DetectRandomMovingFromPercent()
+    {
+        // 処理結果を  bool 値で戻す。randomMovingPercent の値よりも大きければ、false、同じか小さければ true
+        return Random.Range(0, 100) <= randomMovingPercent;
+    }
+
+    /// <summary>
+    /// ランダム値を取得して移動
+    /// </summary>
+    private void ChangeRandomMoveParameters()
+    {
+        // 移動時間をランダム値の範囲で設定
+        duration = Random.Range(durationRange.x, durationRange.y);
+
+        // 移動距離をランダム値の範囲で設定
+        moveDistance = Random.Range(moveDistanceRange.x, moveDistanceRange.y);
+    }
+
+    /// <summary>
+    /// 大きさを変更して点数に反映
+    /// </summary>
+    private void ChangeRandomScales()
+    {
+        // ランダム値の範囲内で大きさを設定
+        int index = Random.Range(0, flowerSizes.Length);
+
+        // 大きさを変更
+        transform.localScale *= flowerSizes[index];
+
+        // 点数を変更
+        point = Mathf.CeilToInt(point * pointRate[index]);   // Mathf.CeilToInt メソッドについて調べてみましょう。
     }
 }
